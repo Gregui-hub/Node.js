2fec2f58e5dc5d70a7e034e297b97fe7abe2160d
Victor Gregui <56194097+Gregui-hub@users.noreply.github.com>
2020-03-14 13:15:18 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server
+})
+
+server.get('/', function (req, res) {
+    return res.render("index")
+})
+
+server.get('/amigos', function (req, res) {
+    return res.render("amigos")
+})
+
+server.get('/contato', function (req, res) {
+    return res.render("contato")
+})
+
+server.listen(5000, function () {
+    console.log('Server is running...')
+})
